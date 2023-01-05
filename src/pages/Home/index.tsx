95677631e4e984750ba3a5a685bdaf5e3e56f14e
Giovanni Fossati <giovannifossati11@gmail.com>
2023-01-05 19:00:05 -0300

    feat: added minutes and seconds calculation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,7 +39,9 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
 
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null); //Armazena o Id do ciclo ativo, podendo ser nulo caso caso não tenha
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0); //Quantidade de segundos que já se passaram
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -65,9 +67,18 @@ export function Home() {
     reset(); // Retorna os valores do form para o defaultValues
   }
 
-  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId); //Procura se o ciclo atual é o ciclo ativo dentro do vetor de Cycles
 
-  console.log(activeCycle)
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0; //Verifica se o clico está ativo, e transforma os minutos inseridos em segundos
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0; // Calcula quantos segundos restam
+
+  const minutesAmount = Math.floor(currentSeconds / 60); //Calcula o número de minutos dentro dos segundos restantes e arredonda pra baixo
+
+  const secondsAmount = currentSeconds % 60; //Calcula quantos segundos restam da divisão acima, pegando os segundo que não cabem em somente uma divisão
+
+  const minutes = String(minutesAmount).padStart(2, "0"); //Define quantos caracteres essa string deve ter, e caso não tenha o número definido, adiciona 0 no começo
+  const seconds = String(secondsAmount).padStart(2, "0");
 
   const task = watch("task");
   const isSubmitDisabled = !task;
@@ -105,11 +116,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
